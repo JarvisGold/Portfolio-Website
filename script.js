2db5f19480d0b70a4f3068c9e63ab558251ea86b
JarvisGold <109941638+JarvisGold@users.noreply.github.com>
2026-06-29 11:14:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Welcome message
+
 console.log("Welcome to Jarvis Portfolio!");
 
-// Show a message when the page loads
 window.onload = function () {
     alert("Welcome to my Personal Portfolio Website!");
 };
 
-// Select all navigation links
 const navLinks = document.querySelectorAll("nav a");
 
-// Add a click event to each navigation link
 navLinks.forEach(function(link) {
     link.addEventListener("click", function() {
         console.log("You clicked: " + link.textContent);
